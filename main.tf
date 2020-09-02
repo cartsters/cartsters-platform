968431236eca19abede4a3e5f28ec40f9ad0d284
Plinio Nascimento <plinio.gsnascimento@gmail.com>
2020-09-02 13:18:08 -0300

    Adding other environments

--- main.tf
+++ main.tf
@@ -6,22 +6,3 @@ terraform {
 provider "azurerm" {
   features {}
 }
-
-resource "azurerm_resource_group" "resource_groups" {
-  name     = var.resource_group_name
-  location = var.location
-}
-
-module "aks" {
-  source              = "Azure/aks/azurerm"
-  resource_group_name = azurerm_resource_group.resource_groups.name
-  client_id           = var.client_id
-  client_secret       = var.client_secret
-  prefix              = var.prefix
-  agents_count        = var.agents_count
-  agents_size         = var.agents_size
-
-  depends_on = [
-      azurerm_resource_group.resource_groups
-  ]
-}
